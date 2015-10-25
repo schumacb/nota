1db80b11a219b5d8f1da611a95ff685e3c61e455
Patrick Kranz <patrick-kranz@live.de>
2015-10-25 21:32:39 +0100

    Fehlender ordner wird nun erstellt

--- deploy.cmd
+++ deploy.cmd
@@ -53,8 +53,14 @@ IF NOT DEFINED DEPLOYMENT_TEMP (
 )
 
 IF NOT DEFINED BIN_PATH (
-  SET BIN_PATH=%ARTIFACTS%\bin
+  SET BIN_PATH=%ARTIFACTS%\..\bin
 )
+
+IF NOT EXIST %BIN_PATH% (
+  echo create bin dir
+  call mkdir %BIN_PATH%
+) 
+
 
 IF NOT DEFINED WYAM_CMD (
   :: Install wyam
