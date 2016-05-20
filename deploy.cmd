a9264807b50865c1fc3976e1b95158f485ab7a7e
Patrick Kranz <patrick-kranz@live.de>
2016-05-20 13:47:42 +0200

    tools folder should be deleted after build

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,8 @@
 @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
 
+
 IF NOT EXIST "Tools" (md "Tools")
 IF NOT EXIST "Tools\Addins" (md "Tools\Addins")
 nuget install Cake -ExcludeVersion -OutputDirectory "Tools"
 .\Tools\Cake\Cake.exe deploy.cake -verbosity=Verbose %*
+IF EXIST "Tools" (rmdir "Tools")
