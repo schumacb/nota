e248c032d78ca91615b283817ace56bc510ad636
Patrick Kranz <patrick-kranz@live.de>
2016-05-21 17:23:13 +0200

    fehler im baue sollten nun auch beim deployen fehlschlagen

--- deploy.cmd
+++ deploy.cmd
@@ -5,4 +5,24 @@ IF NOT EXIST "Tools" (md "Tools")
 IF NOT EXIST "Tools\Addins" (md "Tools\Addins")
 nuget install Cake -ExcludeVersion -OutputDirectory "Tools"
 .\Tools\Cake\Cake.exe deploy.cake -verbosity=Verbose %*
+IF !ERRORLEVEL! NEQ 0 goto error
 IF EXIST "Tools" (rmdir /S /Q "Tools" )
+IF !ERRORLEVEL! NEQ 0 goto error
+
+goto end
+
+:error
+endlocal
+echo An error has occurred during web site deployment.
+call :exitSetErrorLevel
+call :exitFromFunction 2>nul
+
+:exitSetErrorLevel
+exit /b 1
+
+:exitFromFunction
+()
+
+:end
+endlocal
+echo Finished successfully.
