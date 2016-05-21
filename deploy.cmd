79e308af643a0796a53c19d0a0aee72015f559ce
Patrick Kranz <patrick-kranz@live.de>
2016-05-21 17:34:16 +0200

    jetzt aber

--- deploy.cmd
+++ deploy.cmd
@@ -1,13 +1,16 @@
-REM @if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
+@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
 
+IF %ERRORLEVEL% NEQ 0 goto error
 
 IF NOT EXIST "Tools" (md "Tools")
+IF %ERRORLEVEL% NEQ 0 goto error
 IF NOT EXIST "Tools\Addins" (md "Tools\Addins")
+IF %ERRORLEVEL% NEQ 0 goto error
 nuget install Cake -ExcludeVersion -OutputDirectory "Tools"
 .\Tools\Cake\Cake.exe deploy.cake -verbosity=Verbose %*
-IF !ERRORLEVEL! NEQ 0 goto error
+IF %ERRORLEVEL% NEQ 0 goto error
 IF EXIST "Tools" (rmdir /S /Q "Tools" )
-IF !ERRORLEVEL! NEQ 0 goto error
+IF %ERRORLEVEL% NEQ 0 goto error
 
 goto end
 
