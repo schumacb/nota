aeddbc713f6584640c9e73e7548de536d8f22cb5
Patrick Kranz <patrick-kranz@live.de>
2016-07-26 21:55:32 +0200

    tet mit deploy

--- deploy.cmd
+++ deploy.cmd
@@ -2,8 +2,6 @@
 
 IF %ERRORLEVEL% NEQ 0 goto error
 
-IF EXIST "Tools" (rmdir /S /Q "Tools" ) 
-IF %ERRORLEVEL% NEQ 0 goto error
 
 IF NOT EXIST "Tools" (md "Tools")
 IF %ERRORLEVEL% NEQ 0 goto error
@@ -11,6 +9,9 @@ IF NOT EXIST "Tools\Addins" (md "Tools\Addins")
 IF %ERRORLEVEL% NEQ 0 goto error
 nuget install Cake -ExcludeVersion -OutputDirectory "Tools"
 .\Tools\Cake\Cake.exe deploy.cake -verbosity=Verbose %*
+IF %ERRORLEVEL% NEQ 0 goto error
+
+IF EXIST "Tools" (rmdir /S /Q "Tools" ) 
 IF %ERRORLEVEL% NEQ 0 goto error
 
 
