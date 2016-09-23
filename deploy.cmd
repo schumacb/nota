b9efbba702cb85076822866a9dc0ecb68626c903
Patrick Kranz <patrick-kranz@live.de>
2016-09-23 16:22:41 +0200

    nugets cach wird nur auf dem Server gelöscht

--- deploy.cmd
+++ deploy.cmd
@@ -25,9 +25,9 @@ echo An error has occurred during web site deployment.
 IF DEFINED WEBSITE_SITE_NAME (
  echo Try to remove Tools. 
  IF EXIST "Tools" (rmdir /S /Q "Tools" ) 
+ nuget locals all -clear
 )
 
-nuget locals all -clear
 
 call :exitSetErrorLevel
 call :exitFromFunction 2>nul
@@ -43,7 +43,7 @@ endlocal
 IF DEFINED WEBSITE_SITE_NAME (
  echo Try to remove Tools. 
  IF EXIST "Tools" (rmdir /S /Q "Tools" ) 
+ nuget locals all -clear
 )
 
-nuget locals all -clear
 echo Finished successfully.
